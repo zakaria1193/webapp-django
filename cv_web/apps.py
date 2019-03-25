f67058517490f78561df029b6fa364c0ec15ca23
Zakaria Fadli <zakaria.fadli@netatmo.com>
2019-03-25 01:01:16 +0100

    cv_web: add app

---
+++ cv_web/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CvWebConfig(AppConfig):
+    name = 'cv_web'
--- my_django_server/settings.py
+++ my_django_server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+# my own
+    'cv_web'
 ]
 
 MIDDLEWARE = [
